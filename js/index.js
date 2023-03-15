ebf3b3de0be4d0998a60d668b8b95ea50ac4574f
Nadioune <nadia.di.fabio@student.ehb.be>
2023-03-15 16:36:48 +0100

    chat niet gelukt

--- js/index.js
+++ js/index.js
@@ -1,16 +1,22 @@
 "use strict";
 
 const chat = {
-    author: "yourName",
-    init() {
-    },
-    sendMessage() {
-    },
-    fetchMessages() {
-    },
-    renderMessage(message) {
-    }
-
-}
+	author: "yourName",
+	init() {
+		this.fetchMessages();
+	},
+	sendMessage() {},
+	fetchMessages() {
+		fetch("https://dev2chat.onrender.com/messages.json")
+			.then(function (response) {
+				return response.json();
+			})
+			.then(function (messages) {
+				console.log(messages);
+				const render = messages[0].message;
+			});
+	},
+	renderMessage(message) {},
+};
 
 chat.init();
